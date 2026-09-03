9859fd15576d4ddc6785d1fe3514b3f2d35570e1
MesterMan03 <MesterMan03+github@proton.me>
2026-09-03 20:37:23 +0200

    build: use Java 21 for legacy paperweight

--- buildSrc/src/main/kotlin/paperweight-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/paperweight-conventions.gradle.kts
@@ -3,6 +3,14 @@ plugins {
     id("io.papermc.paperweight.userdev")
 }
 
+if (project.name.startsWith("v1_21_")) {
+    paperweight {
+        javaLauncher.set(javaToolchains.launcherFor {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        })
+    }
+}
+
 dependencies {
     compileOnly(project(":bettermodel-api"))
     compileOnly(project(":bettermodel-api:bettermodel-bukkit-api"))
